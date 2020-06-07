96ae1966873c1dcb67a16d7cf22f83efccd50336
Carlos Martinez <carlosm.miquel@gmail.com>
2020-06-07 04:35:49 +0200

    Add templating

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/feed/today")
+def feed(request=Request):
+    return templates.TemplateResponse("feed.html", {
+        "request": request
+    })
